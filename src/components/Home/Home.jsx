a178c318bb38697e2b0d5a0ea2ad639d9cc8a96d
Erick <erickchhuang@gmail.com>
2025-03-26 23:56:37 -0700

    loader code

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,35 +1,53 @@
-import React from 'react'
-import './Home.css'
+import React, { useEffect, useState } from 'react';
+import './Home.css';
 import 'animate.css';
-import { useNavigate } from 'react-router-dom'
-
+import { useNavigate } from 'react-router-dom';
+import Loader from 'react-loaders';
 
 const Home = () => {
-  
+  const [loaderImg, setLoaderImg] = useState(true);
   const navigate = useNavigate();
 
-  return ( 
-    <div className='home-page'>
-      <div> 
-        <h1 className='text'>
-          <span className='animate__animated animate__bounce'>H</span>
-          <span className='animate__animated animate__bounce'>i</span>
-          <br />
-          <div className='logo'>
-            <span>I'm</span>
-            <img className='animate__animated animate__bounce' src="./src/assets/logo.png" alt="text-logo" />
-            <span>rick, </span>
-          </div>
-          <br />
-          <span>Software Developer</span>
-        </h1>
-        <button onClick={() => navigate('/contact/dev')}>Contact Me</button>
-      </div>
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaderImg(false);
+    }, 3000); 
+  }, []);
+
+  return (
+    <>
+    {
+      loaderImg ? (
+        <img className='loader-img' src="/pacman.gif" />
+      ) : (
+        <div className="home-page">
+        <div>
+          <h1 className="text">
+            <span className="animate__animated animate__bounce">H</span>
+            <span className="animate__animated animate__bounce">i</span>
+            <br />
+            <div className="logo">
+              <span>I'm</span>
+              <img
+                className="animate__animated animate__bounce"
+                src="src/assets/logo.png"
+                alt="text-logo"
+              />
+              <span>rick, </span>
+            </div>
+            <br />
+            <span>Software Developer</span>
+          </h1>
+          <button onClick={() => navigate('/contact/dev')}>Contact Me</button>
+        </div>
       <div className="logo-container">
-        <img className="fa-beat-fade" src="./src/assets/logo.png" alt="logo" />
+        <img className="fa-beat-fade" src="src/assets/logo.png" alt="logo" />
       </div>
     </div>
-  )
-}
+      )
+    }
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Portfolio.css'
+import { useEffect, useState } from 'react'
 
 const Portfolio = () => {
+    const [loaderImg, setLoaderImg] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaderImg(false);
+        }, 3000);
+      }, [])
+
   return (
-    <div className='portfolio-page'>
-      <h1>Coming Soon...</h1>
+    <div>
     </div>
   )
 }
