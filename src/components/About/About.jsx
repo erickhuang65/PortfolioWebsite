cb60f5601c86ccd977d659f3a641f84692f72cf7
Erick <erickchhuang@gmail.com>
2025-03-27 00:39:53 -0700

    Merge branch 'main' of https://github.com/erickhuang65/PortfolioWebsite

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,37 +4,54 @@ import 'animate.css'
 import LanguageList from './LanguageList/LanguageList'
 import { useState, useEffect } from 'react'
 import FrameWork from './FrameWork/FrameWork'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
 
+  const [loaderImg, setLoaderImg] = useState(true);
+  const navigate = useNavigate();
   const [currentComponent, setCurrentComponent] = useState("LanguageList");
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaderImg(false);
+    }, 3000); 
+  }, []);
+
   return (
-    <div className='about-page'>
-      <div className='text-container'>
-        <h1 className='title-text'>
-          <span>About Me</span>
-        </h1>
-        <h3 className='intro-text'>
-          <span>I am a results driven Software Developer with a unique background that has prepared me to communicate with team members effectively, approach problems analytically, and actively take the initiative to support team members.</span>
-        </h3> 
-        <div className='graph-text'>
-          <button onClick={() => setCurrentComponent("LanguageList")}>Languages</button>
-          <button onClick={() => setCurrentComponent("FrameWork")}>Frameworks & Technologies</button>
-          {
-          currentComponent === 'LanguageList' ? (
-            <LanguageList />
-          ) : currentComponent === 'FrameWork' ? (
-            <FrameWork />
-          ) : <></> 
-          }
+    <>
+    {
+      loaderImg ? (
+        <img className='loader-img' src="/loader.gif" alt="" />
+      ) : (
+      <div className='about-page'>
+        <div className='text-container'>
+          <h1 className='title-text'>
+            <span>About Me</span>
+          </h1>
+          <h3 className='intro-text'>
+            <span>I am a results driven Software Developer with a unique background that has prepared me to communicate with team members effectively, approach problems analytically, and actively take the initiative to support team members.</span>
+          </h3> 
+          <div className='graph-text'>
+            <button onClick={() => setCurrentComponent("LanguageList")}>Languages</button>
+            <button onClick={() => setCurrentComponent("FrameWork")}>Frameworks & Technologies</button>
+            {
+            currentComponent === 'LanguageList' ? (
+              <LanguageList />
+            ) : currentComponent === 'FrameWork' ? (
+              <FrameWork />
+            ) : <></> 
+            }
+          </div>
+        </div>
+        <div className='image-container'>
+          <h1 className="fa-beat-fade">More to come...</h1>
         </div>
       </div>
-      <div className='image-container'>
-        <h1 className="fa-beat-fade">More to come...</h1>
-      </div>
-    </div>
-  )
+      )
+    }
+    </>
+  );
 }
 
 export default About
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,42 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import './Home.css'
+import React, { useEffect, useState } from 'react';
+import './Home.css';
 import 'animate.css';
-import { useNavigate } from 'react-router-dom'
-
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-
-  const [loading, setLoading] = useState(true);
+  const [loaderImg, setLoaderImg] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, [])
+      setLoaderImg(false);
+    }, 3000); 
+  }, []);
 
-  return ( 
-    <div className='home-page'>
-      <div> 
-        <h1 className='text'>
-          <span className='animate__animated animate__bounce'>H</span>
-          <span className='animate__animated animate__bounce'>i</span>
-          <br />
-          <div className='logo'>
-            <span>I'm</span>
-            <img className='animate__animated animate__bounce' src="/logo.png" alt="text-logo" />
-            <span>rick, </span>
-          </div>
-          <br />
-          <span>Software Developer</span>
-        </h1>
-        <button onClick={() => navigate('/contact/dev')}>Contact Me</button>
-      </div>
+  return (
+    <>
+    {
+      loaderImg ? (
+        <img className='loader-img' src="/loader.gif" />
+      ) : (
+        <div className="home-page">
+        <div>
+          <h1 className="text">
+            <span className="animate__animated animate__bounce">H</span>
+            <span className="animate__animated animate__bounce">i</span>
+            <br />
+            <div className="logo">
+              <span>I'm</span>
+              <img
+                className="animate__animated animate__bounce"
+                src="/logo.png"
+                alt="text-logo"
+              />
+              <span>rick, </span>
+            </div>
+            <br />
+            <span>Software Developer</span>
+          </h1>
+          <button onClick={() => navigate('/contact/dev')}>Contact Me</button>
+        </div>
       <div className="logo-container">
         <img className="fa-beat-fade" src="/logo.png" alt="logo" />
       </div>
     </div>
-  )
-}
+      )
+    }
+    </>
+  );
+};
 
-export default Home
+export default Home;
